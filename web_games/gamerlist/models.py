0eac0170154df2454e670019f0968c2761f77f7e
student-pro <student-pro@b206-6.khpk.ru>
2025-10-28 08:29:24 +0700

    Created models & migrations

--- web_games/gamerlist/models.py
+++ web_games/gamerlist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+class Game(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    release_year = models.IntegerField()
+    image = models.TextField()
+    categories = models.ManyToManyField(Category, related_name="games")
