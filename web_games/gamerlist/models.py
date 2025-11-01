4d10e48228bab8eede010351a556cd80c0312076
student-pro <student-pro@b301-2.khpk.ru>
2025-11-01 10:47:06 +0700

    Formatted the code according to PEP 8 & commented the hell out of it

--- web_games/gamerlist/models.py
+++ web_games/gamerlist/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
 class Category(models.Model):
+    """Категории игр"""
     name = models.CharField(max_length=50)
-    
+
+
 class Game(models.Model):
+    """Игры"""
     name = models.CharField(max_length=150)
     description = models.TextField()
     release_year = models.IntegerField()
+    # Картинка является ссылкой
     image = models.TextField()
     categories = models.ManyToManyField(Category, related_name="games")
--- web_games/gamerlist/views.py
+++ web_games/gamerlist/views.py
@@ -3,64 +3,102 @@ from .models import Game, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
-# Create your views here.
+
 def index(request):
-    
+    """Список игр"""
+
+    # Получение всех игр, категорий и годов выпуска
     games = Game.objects.prefetch_related('categories').all()
     categories = Category.objects.all()
-    years = Game.objects.values_list('release_year', flat=True).distinct().order_by('release_year')
-    
+    years = (Game.objects.values_list('release_year', flat=True)
+             .distinct().order_by('release_year'))
+
+    # Фильтры и поиск
+
+    # Поиск
     search_query = request.GET.get('search', '')
     if search_query:
         games = games.filter(name__icontains=search_query)
 
+    # Фильтр по категории
     category_filter = request.GET.get('category', '')
     if category_filter:
         games = games.filter(categories__id=category_filter)
 
+    # Фильтр по году выпуска
     year_filter = request.GET.get('year', '')
     if year_filter:
         games = games.filter(release_year=year_filter)
 
-    return render(request, 'index.html', {'games': games, 'categories': categories, 'years': years})
+    # Контекст для передачи в шаблон
+    context = {
+        'games': games,
+        'categories': categories,
+        'years': years
+    }
+
+    return render(request, 'index.html', context)
+
 
 def profile(request):
+    """Страница профиля пользователя"""
+
     return render(request, 'profile.html')
 
+
 def register_page(request):
-    
+    """Страница регистрации"""
+
     if request.method == 'POST':
+        # Получение введенных данных
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
         confirm_password = request.POST.get('confirm_password')
 
+        # Подтверждение пароля
         if password == confirm_password:
-            user = User.objects.create_user(username=username, email=email, password=password)
+            # Регистрация и вход
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
             login(request, user)
             return redirect('index')
         else:
-            return render(request, 'register.html', {'error': 'Пароли не совпадают.'})
-        
+            # Вывод ошибки
+            return render(request, 'register.html',
+                          {'error': 'Пароли не совпадают.'})
+
     return render(request, 'register.html')
 
+
 def login_page(request):
-    
+    """Страница входа"""
+
     if request.method == 'POST':
+        # Получение введенных данных
         username = request.POST.get('username')
         password = request.POST.get('password')
-        
+
+        # Попытка аутентификации
         user = authenticate(request, username=username, password=password)
         if user is not None:
+            # Вход и переход к списку игр
             login(request, user)
             return redirect('index')
         else:
-            return render(request, 'login.html', {'error': 'Неверное имя пользователя или пароль.'})
+            # Вывод ошибки
+            return render(request, 'login.html',
+                          {'error': 'Неверное имя пользователя или пароль.'})
 
     return render(request, 'login.html')
 
+
 def logout_page(request):
-    
+    """Выход из аккаунта"""
+
     logout(request)
-    
+
     return redirect('index')
